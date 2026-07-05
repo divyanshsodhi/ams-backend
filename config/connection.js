45852aede2234ac321fb586b869e9024aa4d1f6b
Divyansh Sodhi <sodhidivyansh80@gmail.com>
2026-07-05 16:48:42 +0530

    redesigned: login page and dashboard.

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,20 @@
-    const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-    const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(
-        process.env.MONGODB_URI
-        );
+const connectDB = async () => {
+  try {
+    const uri = process.env.MONGODB_URI;
 
-        console.log(
-        `MongoDB Connected: ${connectionInstance.connection.host}`
-        );
-    } catch (error) {
-        console.error("MongoDB Connection Error:", error.message);
+    const connectionInstance = await mongoose.connect(uri, {
+      tls: true,
+      tlsInsecure: true,
+    });
 
-        // Exit the process if DB connection fails
-        process.exit(1);
-    }
-    };
+    console.log(`MongoDB Connected: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error.message);
 
-    module.exports = connectDB;
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
